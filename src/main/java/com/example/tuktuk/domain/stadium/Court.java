5e928dc42cc94cb63b474f6db7d7607fee9a9a21
Hoo-sung <gntjd135@gmail.com>
2024-01-05 18:20:11 +0900

    feat(global): City, Province Enum 생성
     static 상수 Enum으로 관리

--- src/main/java/com/example/tuktuk/domain/stadium/Court.java
+++ src/main/java/com/example/tuktuk/domain/stadium/Court.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Court {
 
+    String Name;
     CourtType courtType;
     int hourlyRentFee;
-    List<byte[]> images;  // 이미지 데이터를 바이트 배열로 저장
+    List<String> images;  // 이미지 데이터를 바이트 배열로 저장
 }
--- src/main/java/com/example/tuktuk/domain/stadium/Stadium.java
+++ src/main/java/com/example/tuktuk/domain/stadium/Stadium.java
@@ -9,7 +9,7 @@ public class Stadium {
 
     String Name;
     Member owner;
-    String location;//지번까지의 상세한 주소
+    Location location;
     String specificInfo; //주차 여부, 찾아 오는 상세한 길 같은 구단 등록주가 상세히 입력하는 폼.
     List<Court> courts;
 }
