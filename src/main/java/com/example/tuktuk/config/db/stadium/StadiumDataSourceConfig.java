893e792e4da7b45c320a9baf424a872e61a00d4e
Hoo-sung <gntjd135@gmail.com>
2024-01-11 16:40:42 +0900

    feat(StadiumDataSourceConfig): StadiumDataSourceConfig 설정 정보 파일 작성 #16

--- src/main/java/com/example/tuktuk/config/db/stadium/StadiumDataSourceConfig.java
+++ src/main/java/com/example/tuktuk/config/db/stadium/StadiumDataSourceConfig.java
@@ -9,9 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class StadiumDataSourceConfig extends BasicDataSourceConfig {
@@ -27,7 +30,63 @@ public class StadiumDataSourceConfig extends BasicDataSourceConfig {
         super(env);
     }
 
+    @Profile(AppProfile.NON_TEST)
+    @Bean(name = DATASOURCE)
+    public DataSource dataSource(
+            @Qualifier(StadiumDataSourceConfig.ROUTE_DATASOURCE) DataSource routeDataSource
+    ) {
+        return new LazyConnectionDataSourceProxy(routeDataSource);
+    }
 
+    @Profile(AppProfile.NON_TEST)
+    @Bean(name = ROUTE_DATASOURCE)
+    public DataSource routingDataSource(
+            @Qualifier(StadiumDataSourceConfig.RW_DATASOURCE) DataSource rwDataSource,
+            @Qualifier(StadiumDataSourceConfig.RO_DATASOURCE) DataSource roDataSource
+    ) {
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+        dataSourceMap.put(DataSourceRole.READ_WRITE, rwDataSource);
+        dataSourceMap.put(DataSourceRole.READ_ONLY, roDataSource);
+
+        return new StadiumRouteDataSource(dataSourceMap);
+    }
+
+    @Profile(AppProfile.NON_TEST)
+    @Bean(name = RW_DATASOURCE)
+    public DataSource rwDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(getJdbcUrl(dataSourceType, DataSourceRole.READ_WRITE));
+        dataSource.setDriverClassName(getDriverClassName(dataSourceType));
+        dataSource.setUsername(getUsername(dataSourceType));
+        dataSource.setPassword(getPassword(dataSourceType));
+
+        return dataSource;
+    }
+
+    @Profile(AppProfile.NON_TEST)
+    @Bean(name = RO_DATASOURCE)
+    public DataSource roDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(getJdbcUrl(dataSourceType, DataSourceRole.READ_ONLY));
+        dataSource.setDriverClassName(getDriverClassName(dataSourceType));
+        dataSource.setUsername(getUsername(dataSourceType));
+        dataSource.setPassword(getPassword(dataSourceType));
+
+        return dataSource;
+    }
+
+
+    @Profile(AppProfile.TEST)
+    @Bean(name = TEST_DATASOURCE)
+    public DataSource testDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(getJdbcUrl(dataSourceType, DataSourceRole.TEST_ONLY));
+        dataSource.setDriverClassName(getDriverClassName(dataSourceType));
+        dataSource.setUsername(getUsername(dataSourceType));
+        dataSource.setPassword(getPassword(dataSourceType));
+
+        return dataSource;
+    }
 
 
 }
