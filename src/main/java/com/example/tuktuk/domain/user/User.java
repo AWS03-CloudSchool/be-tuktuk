fe2e6280364820f326dc7088718dab6b8456df92
Hoo-sung <gntjd135@gmail.com>
2024-01-13 13:59:15 +0900

    refactor(all): @Comment 어노테이션 필수적이지 않은 부분 제거 #2

--- src/main/java/com/example/tuktuk/domain/user/User.java
+++ src/main/java/com/example/tuktuk/domain/user/User.java
@@ -23,26 +23,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Comment("This column is email address and is provided by SNS")
-    @Column(name = "mail", nullable = false, length = 255)
-    private String mail;
+    @Column(name = "email", nullable = false, length = 255)
+    private String email;
 
     @Embedded
     private Password password;
 
-    @Comment("This column is user name and is provided by SNS")
     @Column(name = "nick_name", nullable = false, length = 16)
     private String nickName;
 
-    @Comment("This column is user birthday and is provided by SNS")
     @Column(name = "profile_birth", nullable = false, length = 16)
     private String profileBirth;
 
-    @Comment("This column is user gender and is provided by SNS")
     @Column(name = "gender", nullable = false, length = 3)
     private String gender;
 
-    @Comment("This column is user phone number and is provided by SNS")
     @Column(name = "tel_no", nullable = false, length = 36)
     private String telNo;
 
@@ -55,7 +50,6 @@ public class User {
     @Enumerated
     @Column(name = "role", nullable = false)
     private Role role;
-
 
     @Comment("This column is social login provider (kakao, google, naver)")
     @Column(name = "provider", nullable = false, length = 36)
