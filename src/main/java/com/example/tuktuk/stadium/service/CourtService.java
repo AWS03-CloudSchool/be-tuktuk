84e86c61c62f572d91c4ae85dc7260c4bc045a22
NotableDeveloper <castlereal01@gmail.com>
2024-01-17 15:27:10 +0900

    feat(court) : CourtService 최초 작성 #22 CourtService의 프로토타입 작성

---
+++ src/main/java/com/example/tuktuk/stadium/service/CourtService.java
@@ -0,0 +1,26 @@
+package com.example.tuktuk.stadium.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class CourtService {
+  public void findByCourtId(){
+
+  }
+
+  public void saveCourt(){
+
+  }
+
+  public void updateCourt(){
+
+  }
+
+  public void deleteCourt(){
+
+  }
+}
