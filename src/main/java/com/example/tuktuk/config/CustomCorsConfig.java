f0427c9c892de5d424d9c6e6e114de0b9f9829ea
Hoo-sung <gntjd135@gmail.com>
2024-02-11 14:18:31 +0900

    fix(CORS): CORS 설정 수정 #53
    - OPTIONS의 경우, CORS 에러 해결

--- src/main/java/com/example/tuktuk/config/CustomCorsConfig.java
+++ src/main/java/com/example/tuktuk/config/CustomCorsConfig.java
@@ -2,6 +2,7 @@ package com.example.tuktuk.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,11 +11,28 @@ public class CustomCorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/users")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("POST","OPTIONS") // POST 메소드만 허용
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+
+        registry.addMapping("/fieldowners")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("POST","OPTIONS") // POST 메소드만 허용
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+                .maxAge(3000) // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+                .exposedHeaders(HttpHeaders.AUTHORIZATION);
+
+
     }
 
 }
