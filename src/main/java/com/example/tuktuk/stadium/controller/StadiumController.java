1fed3cb6d3c10c8f37139debd5aa904c2294a63c
Hoo-sung <gntjd135@gmail.com>
2024-02-13 21:19:16 +0900

    fix(Cors): Content-type 헤더 설정 #53

--- src/main/java/com/example/tuktuk/stadium/controller/StadiumController.java
+++ src/main/java/com/example/tuktuk/stadium/controller/StadiumController.java
@@ -13,7 +13,10 @@ import com.example.tuktuk.stadium.service.StadiumService;
 import com.example.tuktuk.security.SecurityContextHolderUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
@@ -60,17 +63,27 @@ public class StadiumController {
 
     @Secured("FIELD_OWNER")
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public StadiumCreateResponseDto createStadium(@RequestBody StadiumCreateRequestDto requestDto) {
+    public ResponseEntity<StadiumCreateResponseDto> createStadium(@RequestBody StadiumCreateRequestDto requestDto) {
         String ownerId = SecurityContextHolderUtil.getUserId();
-        return stadiumService.saveStadium(ownerId, requestDto);
+        StadiumCreateResponseDto responseDto = stadiumService.saveStadium(ownerId, requestDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return new ResponseEntity<>(responseDto, headers, HttpStatus.CREATED);
     }
 
     @Secured("FIELD_OWNER")
     @PatchMapping(value = "/{stadiumId}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public StadiumUpdateResponseDto updateStadium(@PathVariable(name = "stadiumId") long stadiumId,
-                                                  @RequestBody StadiumUpdateRequestDto requestDto) {
+    public ResponseEntity<StadiumUpdateResponseDto> updateStadium(@PathVariable(name = "stadiumId") long stadiumId,
+                                                                  @RequestBody StadiumUpdateRequestDto requestDto) {
         String ownerId = SecurityContextHolderUtil.getUserId();
-        return stadiumService.updateStadium(ownerId, stadiumId, requestDto);
+        StadiumUpdateResponseDto responseDto = stadiumService.updateStadium(ownerId, stadiumId, requestDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return new ResponseEntity<>(responseDto, headers, HttpStatus.OK);
     }
 
     @Secured("FIELD_OWNER")
