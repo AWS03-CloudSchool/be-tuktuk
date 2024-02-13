7dd8a16d14ca081c9c5ac598edbfc22dc5c74d30
Hoo-sung <gntjd135@gmail.com>
2024-02-13 23:59:10 +0900

    fix(Cors): Cors preflight 오류 수정 #53

--- src/main/java/com/example/tuktuk/config/CustomCorsConfig.java
+++ src/main/java/com/example/tuktuk/config/CustomCorsConfig.java
@@ -24,6 +24,18 @@ public class CustomCorsConfig implements WebMvcConfigurer {
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
 
+        registry.addMapping("/stadiums")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("POST","OPTIONS") // POST 메소드만 허용
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+
+        registry.addMapping("/courts")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("POST","OPTIONS") // POST 메소드만 허용
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
