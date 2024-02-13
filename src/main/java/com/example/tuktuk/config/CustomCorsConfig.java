a0a85547056cdf8eccd04b7f4383fa63062bb18f
Hoo-sung <gntjd135@gmail.com>
2024-02-14 02:15:40 +0900

    fix(Cors): Cors 수정 V4 #53

--- src/main/java/com/example/tuktuk/config/CustomCorsConfig.java
+++ src/main/java/com/example/tuktuk/config/CustomCorsConfig.java
@@ -12,19 +12,34 @@ public class CustomCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://www.tukktukk.com")
-                .allowedMethods("GET", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization")
+        registry.addMapping("/users")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("POST","OPTIONS") // POST 메소드만 허용
                 .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000) // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
-                .exposedHeaders(HttpHeaders.AUTHORIZATION);
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+
+        registry.addMapping("/fieldowners")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("POST","OPTIONS") // POST 메소드만 허용
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+
+        registry.addMapping("/stadiums")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("POST","OPTIONS") // POST 메소드만 허용
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+
+        registry.addMapping("/courts")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("POST","OPTIONS") // POST 메소드만 허용
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://www.tukktukk.com")
-                .allowedMethods("POST", "PUT", "PATCH", "OPTIONS")
+                .allowedOrigins("http://localhost:8080", "http://localhost:3000", "https://www.tukktukk.com")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization")
-                .allowedHeaders("Content-Type")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000) // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
                 .exposedHeaders(HttpHeaders.AUTHORIZATION);
