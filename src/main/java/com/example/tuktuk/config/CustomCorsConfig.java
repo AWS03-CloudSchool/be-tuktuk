e35d5cf86160294c11ce04f962e6c38b2ab56c6c
Hoo-sung <gntjd135@gmail.com>
2024-02-14 23:27:56 +0900

    fix(Cors): CORS 설정 리펙토링 #53

--- src/main/java/com/example/tuktuk/config/CustomCorsConfig.java
+++ src/main/java/com/example/tuktuk/config/CustomCorsConfig.java
@@ -14,22 +14,11 @@ public class CustomCorsConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://www.tukktukk.com")
-                .allowedMethods("GET", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000) // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+                .maxAge(30000) // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
                 .exposedHeaders(HttpHeaders.AUTHORIZATION);
-
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://www.tukktukk.com")
-                .allowedMethods("POST", "PUT", "PATCH", "OPTIONS")
-                .allowedHeaders("Authorization")
-                .allowedHeaders("Content-Type")
-                .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000) // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
-                .exposedHeaders(HttpHeaders.AUTHORIZATION);
-
-
     }
 
 }
