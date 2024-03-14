d458b77df2cabef9ffed4f764cb20be75ee41bee
NotableDeveloper <castlereal01@gmail.com>
2024-03-14 15:28:27 +0900

    fix : AccessControlMap 구현체 변경 HashMap에서 LinkedHashMap으로 변경

--- src/main/java/com/example/tuktuk/security/AccessControlMap.java
+++ src/main/java/com/example/tuktuk/security/AccessControlMap.java
@@ -1,5 +1,6 @@
 package com.example.tuktuk.security;
 
+import java.util.LinkedHashMap;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -10,12 +11,12 @@ public class AccessControlMap {
     /*
       HTTP Method, Allow URI 의 쌍으로 허용할 URI를 저장한다.
     */
-    private final Map<AccessControlRecord, Boolean> accessControlMap = new HashMap<>();
+    private final Map<AccessControlRecord, Boolean> accessControlMap = new LinkedHashMap<>();
 
     public AccessControlMap() {
+        accessControlMap.put(AccessControlRecord.from("GET", "/"), Boolean.TRUE);
         accessControlMap.put(AccessControlRecord.from("GET", "/my-stadiums"), Boolean.FALSE);
         accessControlMap.put(AccessControlRecord.from("GET", "/my-schedules"), Boolean.FALSE);
-        accessControlMap.put(AccessControlRecord.from("GET", "/"), Boolean.TRUE);
         accessControlMap.put(AccessControlRecord.from("POST", "/users"), Boolean.TRUE);
         accessControlMap.put(AccessControlRecord.from("POST", "/fieldowners"), Boolean.TRUE);
     }
